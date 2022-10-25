65f14fd24b66037a0f8354b0de74f3b4c115a1e2
Daniel Puyo <danielpuyo25@gmail.com>
2022-10-25 17:36:57 -0300

    first Upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+//eleciona a Data local e separa os parametros especificos de hora e hora completa//
+var tempo = new Date
+var hora = tempo.getHours()
+var minutos = tempo.toLocaleTimeString()
+
+
+//captura o elemento (ID) através do DOM para modificar os atributos//
+var imagem = window.document.getElementById('imagem')
+var div = window.document.getElementById('msg') 
+
+// Substitui o texto do conteúdo capturado//
+div.innerHTML = `Agora são ${minutos} `
+
+//Funcão que definine uma imagem e cor de fundo a Cada Hora do Dia//
+function atualizaFoto(){
+    if (hora >= 6 && hora < 12) {
+        imagem.src = 'manha.jpg'
+        document.body.style.background = '#E1BE05'
+        
+    }
+    else if (hora >= 12 && hora <= 18) {
+        imagem.src = 'tarde.jpg'
+        document.body.style.background = '#C96416'
+    }
+    else {
+        imagem.src = 'noite.jpg'
+        document.body.style.background = "#532F49"
+    }
+
+}
+
+
+
+
+
+
+
+
