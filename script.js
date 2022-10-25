b12d010205b21659ad5895da696f06643535aea3
Daniel Puyo <danielpuyo25@gmail.com>
2022-10-25 18:14:22 -0300

    update size

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ var tempo = new Date
 var hora = tempo.getHours()
 var minutos = tempo.toLocaleTimeString()
 
-
 //captura o elemento (ID) através do DOM para modificar os atributos//
 var imagem = window.document.getElementById('imagem')
 var div = window.document.getElementById('msg') 
+var saudacao = window.document.getElementById('saudacao')
 
 // Substitui o texto do conteúdo capturado//
 div.innerHTML = `Agora são ${minutos} `
@@ -17,15 +17,18 @@ function atualizaFoto(){
     if (hora >= 6 && hora < 12) {
         imagem.src = 'manha.jpg'
         document.body.style.background = '#E1BE05'
+        saudacao.innerHtml = 'Bom Dia!'
         
     }
     else if (hora >= 12 && hora <= 18) {
         imagem.src = 'tarde.jpg'
         document.body.style.background = '#C96416'
+        saudacao.innerHTML = 'Boa Tarde!'
     }
     else {
         imagem.src = 'noite.jpg'
         document.body.style.background = "#532F49"
+        saudacao.innerHTML = 'Boa Noite'
     }
 
 }
